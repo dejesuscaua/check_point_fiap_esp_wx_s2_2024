7fb71b667315860d99b49da46967b0ebf383c7d5
Cauã Gonçalves de Jesus <84869963+dejesuscaua@users.noreply.github.com>
2024-10-09 22:36:13 -0300

    Update

--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
@@ -16,6 +16,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Grupo Cauã e Carlos");
+        System.out.println("Grupo Cauã e Carlos Projetos");
     }
 }

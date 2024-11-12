b934398114b547f2028aaeec3efc713806f9cba9
Cauã Gonçalves de Jesus <84869963+dejesuscaua@users.noreply.github.com>
2024-11-11 21:42:59 -0300

    Notes by Caua

--- checkpoint_3/libunclealegnment/src/main/java/br/com/fiap/twoespwx/libunclealegnment/core/EuclidianDistance.java
+++ checkpoint_3/libunclealegnment/src/main/java/br/com/fiap/twoespwx/libunclealegnment/core/EuclidianDistance.java
@@ -6,10 +6,10 @@ import br.com.fiap.twoespwx.libunclealegnment.api.input.DistanceInput;
 
 public class EuclidianDistance extends BaseDistance {
 
-    private static final String METHOD = "EUCLIDIAN_DISTANCE";
+    private static final String METHOD = "EUCLIDIAN_DISTANCE"; // Define uma constante METHOD, com o valor "EUCLIDIAN_DISTANCE". static final indica que é uma constante de classe e seu valor não será alterado.
     private static final String ABOUT = """
         WORKING IN PROGRESS
-    """;
+    """; // Define a constante ABOUT, que contém uma breve descrição, neste caso "WORKING IN PROGRESS". e o static final indica que é uma constante de classe e seu valor não será alterado tambem.
 
     private static final List<String> REFERENCES = List.of(
         "https://en.wikipedia.org/wiki/Euclidian_distance"
@@ -22,6 +22,6 @@ public class EuclidianDistance extends BaseDistance {
     @Override
     public DistanceResult run(DistanceInput input) {
         throw new UnsupportedOperationException("Unimplemented method 'run'");
-    }
+    } // O método run recebe um parâmetro input do tipo DistanceInput, que provavelmente contém os dados necessários para o cálculo da distância.
 
 }

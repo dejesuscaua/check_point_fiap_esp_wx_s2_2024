5799327db7191041598d33369a45c9b939eb6408
Cauã Gonçalves de Jesus <84869963+dejesuscaua@users.noreply.github.com>
2024-11-11 21:39:37 -0300

    notes by Caua

--- checkpoint_3/libunclealegnment/src/main/java/br/com/fiap/twoespwx/libunclealegnment/core/BaseDistance.java
+++ checkpoint_3/libunclealegnment/src/main/java/br/com/fiap/twoespwx/libunclealegnment/core/BaseDistance.java
@@ -4,26 +4,27 @@ import java.util.List;
 
 public abstract class BaseDistance implements DistanceCalculator {
     
-    private String method;
-    private String about;
-    private List<String> references;
+    private String calculationMethod; // Ultiliza uma String que armazena o método de cálculo de distância.
+    private String description; // é uma String que armazena a descrição do método de cálculo.
+    private List<String> sourceReferences; // uma lista que armazena referências sobre o método, como fontes ou bibliografia.
 
-    public BaseDistance(String method, String about, List<String> references) {
-        this.method = method;
-        this.about = about;
-        this.references = references;
+    public BaseDistance(String calculationMethod, String description, List<String> sourceReferences) {
+        this.calculationMethod = calculationMethod;
+        this.description = description;
+        this.sourceReferences = sourceReferences;
+        // Atribui os parâmetros recebidos pelo construtor (calculationMethod, description, e sourceReferences).
     }
 
-    public String getMethod() {
-        return method;
+    public String getCalculationMethod() {
+        return calculationMethod;
     }
 
-    public String getAbout() {
-        return about;
+    public String getDescription() {
+        return description;
     }
 
-    public List<String> getReferences() {
-        return references;
+    public List<String> getSourceReferences() {
+        return sourceReferences;
     }
-
+    // Tranformando em publico  permitindo que outras classes acessem
 }
